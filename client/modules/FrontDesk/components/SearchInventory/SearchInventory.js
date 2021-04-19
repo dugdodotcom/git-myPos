3fbe437f94161c58ede6d83c6b1189f3bc53c7ca
Alfado Puja Kusuma <srontobisnis@gmail.com>
2021-04-19 15:46:13 +0700

    push main page

--- client/modules/FrontDesk/components/SearchInventory/SearchInventory.js
+++ client/modules/FrontDesk/components/SearchInventory/SearchInventory.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types'
 function SearchInventory (props) {
   return (
     <div className="row">
-    <div className="col-sm">
-      One of three columns
-    </div>
-    <div className="col-sm">
-      One of three columns
-    </div>
-    <div className="col-sm">
-      One of three columns
-    </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
   </div>
   )
 }
